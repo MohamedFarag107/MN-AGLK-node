aed0fc40972bc2aaba751e4f797cba516e5eec37
Islam Sayed Galal <eslamgalal0312@gmail.com>
2023-03-25 21:07:24 +0200

    comic commit

--- middleware/uploadImageMiddleWare.js
+++ middleware/uploadImageMiddleWare.js
@@ -23,26 +23,3 @@ const multerObject = ()=>{
 
 exports.uploadSingleImage = (fieldName)=>
     multerObject().single(fieldName);
-
-
-// exports.uploadMixOfImages = (arrayOfFields) =>
-//     multerObject().fields(arrayOfFields);
-
-
-
-// const storage = multer.diskStorage({destination: function (req, file, cb) {
-//         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//             cb(null, 'uploads/BooksImage/')
-//         } else if (file.mimetype === 'application/pdf') {
-//             cb(null, 'uploads/BooksFile/')
-//         } else {
-//             cb({ message: 'Unsupported file format' }, false);
-//         }
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname)
-//     }
-// });
-
-// const upload = multer({ storage: storage });
-// exports.uploadSingleImage = (fieldName)=>  upload.single(fieldName);
